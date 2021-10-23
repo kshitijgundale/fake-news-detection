3cd5e4ed1319b31b1bbf34c99aafd43db8d1188c
kshitijgundale <kshitijgundale08@gmail.com>
2021-10-23 11:44:20 +0530

    Added build_node_features, fixed build_edges

--- propagation_graph.py
+++ propagation_graph.py
@@ -1,17 +1,21 @@
 import os
 import json
+from nltk.sentiment.vader import VaderConstants
 import pandas as pd
 from utils import *
 import networkx as nx
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 class PropagationGraphBuilder():
     '''
     Each tweet is assumed to be in seperate json file in tweets folder and retweets for all tweets are in single json file named retweets.json
     '''
 
-    def __init__(self, news_id, news_path) -> None:
+    def __init__(self, news_id, news_path, retweet_gap, vader: SentimentIntensityAnalyzer) -> None:
         self.news_path = news_path
         self.news_id = news_id
+        self.retweet_gap = retweet_gap
+        self.vader = vader
         self.num_of_tweets = None
         self.all_retweets = None
         self.tweets = None
@@ -21,6 +25,9 @@ class PropagationGraphBuilder():
         '''
         Builds edges of propagation graphs, given tweets and their retweets.
         '''
+
+        # Build news node
+        news_node = json.load(os.path.join(self.news_path, 'news_article.json'))
 
         # Sorting tweets based on timestamp
         tweets = self.collect_all_tweets()
@@ -64,8 +71,14 @@ class PropagationGraphBuilder():
                     for j in range(0, i):
                         candidate_source = retweets[j]
 
-                        if target_retweet['user']['id_str'] in [x['id_str'] for x in target_tweet['entities']['user_mentions']]:
+                        # 1. Check if user of candidate_source mentions user of target tweet
+                        # 2. Check if target target was published within n hours of candidate source
+                        cond_a = target_retweet['user']['id_str'] in [x['id_str'] for x in target_tweet['entities']['user_mentions']]
+                        cond_b = ((target_retweet['created_at'] - candidate_source['created_at'])/3600) < self.retweet_gap 
+
+                        if cond_a or cond_b:
                             source = candidate_source
+                            break
 
                     if source is None:
                         edges.append((target_tweet['id_str'], target_retweet['id_str']))
@@ -73,13 +86,15 @@ class PropagationGraphBuilder():
                         edges.append((source['id_str'], target_retweet['id_str']))
 
         # save edges to txt file
-        with open(os.path.join(save_location, f'{self.news_id}_graph.txt'), 'w') as f:
-            json.dump(edges, f)
+        if save_to_file:
+            with open(os.path.join(save_location, f'{self.news_id}_graph.txt'), 'w') as f:
+                json.dump(edges, f)
 
         # Create networkx graph with tweets/retweets as nodes
         # and keys of tweet/retweet object as node attributes for easy access 
         # when building node features
         g = nx.DiGraph()
+        g.add_node(self.news_id, **news_node)
         g.add_nodes_from([
             (x['id_str'], x) for x in tweets
         ])
@@ -95,17 +110,74 @@ class PropagationGraphBuilder():
         '''
 
         feature_names = [
-            'account_age'
+            'source_time_diff'
+            'account_age',
+            'parent_time_diff'
+            'avg_child_time_diff',
+            'is_verified',
+            'num_friends',
+            'num_followers',
+            'num_hastags',
+            'num_mentions',
+            'vader_score'
         ]
         data = []
         
         tweets = self.collect_all_tweets()
+        source = self.graph.nodes[self.news_id] if self.graph.nodes[self.news_id]['publish_date'] else tweets[0]
         for tweet in tweets:
+
+            id = tweet['id_str']
+            
             # Temporal Features
+            source_time_diff = tweet['created_at'] - source['publish_date']
             account_age = get_account_age(tweet['created_at'], tweet['user']['created_at'])
+            parent_time_dff = get_parent_time_diff(self.graph, self.news_id, tweet['id_str'], tweet['created_at'])
+            avg_child_time_diff = get_avg_child_time_diff(self.graph, tweet['id_str'], tweet['created_at'])
+
+            # User-based features
+            is_verified = 1 if tweet['user']['verified'] else 0
+            num_friends = tweet['user']['friends_count']
+            num_followers = tweet['user']['followers_count']
             
-            pass
-            
+            # Text-based features
+            num_hastags = len(tweet['entities']['hashtags'])
+            num_mentions = len(tweet['entities']['user_mentions'])
+            vader_score = get_vader_score(tweet['text'])
+
+            data.append([
+                id, source_time_diff, account_age, parent_time_dff,
+                avg_child_time_diff, is_verified, num_friends, num_followers,
+                num_hastags, num_mentions, vader_score
+            ])
+
+            # Extracting features for retweets
+            for retweet in self.all_retweets[tweet['id_str']]:
+
+                id = retweet['id_str']
+
+                # Temporal Features
+                source_time_diff = retweet['created_at'] - source['publish_date']
+                account_age = get_account_age(retweet['created_at'], retweet['user']['created_at'])
+                parent_time_dff = get_parent_time_diff(self.graph, self.news_id, retweet['id_str'], retweet['created_at'])
+                avg_child_time_diff = get_avg_child_time_diff(self.graph, retweet['id_str'], retweet['created_at'])
+
+                # User-based features
+                is_verified = 1 if retweet['user']['verified'] else 0
+                num_friends = retweet['user']['friends_count']
+                num_followers = retweet['user']['followers_count']
+                
+                # Text-based features
+                num_hastags = len(retweet['entities']['hashtags'])
+                num_mentions = len(retweet['entities']['user_mentions'])
+                vader_score = get_vader_score(retweet['text'])
+
+                data.append([
+                    id, source_time_diff, account_age, parent_time_dff,
+                    avg_child_time_diff, is_verified, num_friends, num_followers,
+                    num_hastags, num_mentions, vader_score
+                ])
+
         return pd.DataFrame(data=data, columns=feature_names)
 
 
